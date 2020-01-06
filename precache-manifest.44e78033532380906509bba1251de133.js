278594a50faf7e134aa2a08b6987020e71db6106
yyuri <oriol.goma@gmail.com>
2020-01-06 21:10:18 +0100

    Updates

---
+++ precache-manifest.44e78033532380906509bba1251de133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a796bb5247536c9c32ccc832d4446a1",
+    "url": "/liltest/index.html"
+  },
+  {
+    "revision": "5c99e780587925e3e508",
+    "url": "/liltest/static/css/main.cc813e85.chunk.css"
+  },
+  {
+    "revision": "7018cc3921a538224557",
+    "url": "/liltest/static/js/2.8adfdecf.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/liltest/static/js/2.8adfdecf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c99e780587925e3e508",
+    "url": "/liltest/static/js/main.5c4a9360.chunk.js"
+  },
+  {
+    "revision": "34c020d89cebc5703918",
+    "url": "/liltest/static/js/runtime-main.3edf92c1.js"
+  }
+]);
